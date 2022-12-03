3eded10d737c3d5003627cbee1271772d25d219c
Elvira <rostechno_rb@mail.ru>
2022-12-03 20:21:20 +0500

    Task1 corrected

--- src/Homework12/Main12.java
+++ src/Homework12/Main12.java
@@ -9,6 +9,9 @@ public class Main12 {
         Author aelitaAuthor = new Author("Алексей", "Толстой");
         Book aelita = new Book("Аэлита", 1995, aelitaAuthor);
 
+        /*if (warAndPeace.getName().equals(null) && warAndPeaceAuthor.getSurname().equals(null)){
+            throw new IllegalArgumentException("Неуказано наименование книги");
+        }*/
         System.out.println("'" + warAndPeace.getName() + "' " + warAndPeace.getYearPublication() + " года издания" + " автор " + warAndPeaceAuthor.getName() + " "+ warAndPeaceAuthor.getSurname());
         System.out.println("'" + aelita.getName() + "' " + aelita.getYearPublication() + " года издания"  + " автор " + aelitaAuthor.getName() + " "+ aelitaAuthor.getSurname());
 
