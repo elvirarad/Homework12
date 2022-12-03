da604f247dfc31da7966a98f7bf527274711ac9f
Elvira <rostechno_rb@mail.ru>
2022-12-03 19:53:03 +0500

    Task1 corrected

--- src/Homework12/Book.java
+++ src/Homework12/Book.java
@@ -2,17 +2,21 @@ package Homework12;
 
 public class Book {
     private String name;
-    //Author;
     private int yearPublication;
 
-    public Book (String name, int yearPublication){
+    private Author author;
+
+    public Book (String name, int yearPublication, Author author){
         this.name = name;
         this.yearPublication = yearPublication;
+        this.author = getAuthor();
         }
 
     public String getName() {return this.name;}
 
     public int getYearPublication() {return this.yearPublication;}
+
+    public Author getAuthor() {return this.author;}
 
     public void setYearPublication(int yearPublication) {
         this.yearPublication = yearPublication;
--- src/Homework12/Main12.java
+++ src/Homework12/Main12.java
@@ -1,19 +1,13 @@
 package Homework12;
 
 public class Main12 {
-    /*public static Book CreateBook (String name, int yearPublication){
-        Book book = new Book();
-        Book.name = name;
-        Book.yearPublication = yearPublication;
-        return book;
-    }*/
     public static void main(String[] args) {
         //объекты и классы
-        Book warAndPeace = new Book("Война и мир", 2001);
         Author warAndPeaceAuthor = new Author("Лев", "Толстой");
+        Book warAndPeace = new Book("Война и мир", 2001, warAndPeaceAuthor);
 
-        Book aelita = new Book("Аэлита", 1995);
         Author aelitaAuthor = new Author("Алексей", "Толстой");
+        Book aelita = new Book("Аэлита", 1995, aelitaAuthor);
 
         System.out.println("'" + warAndPeace.getName() + "' " + warAndPeace.getYearPublication() + " года издания" + " автор " + warAndPeaceAuthor.getName() + " "+ warAndPeaceAuthor.getSurname());
         System.out.println("'" + aelita.getName() + "' " + aelita.getYearPublication() + " года издания"  + " автор " + aelitaAuthor.getName() + " "+ aelitaAuthor.getSurname());
